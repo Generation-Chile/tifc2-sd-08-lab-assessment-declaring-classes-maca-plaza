3ea8d7f3ac1ae1a787a9547c51770876efe600a2
maca-plaza <165829605+maca-plaza@users.noreply.github.com>
2024-04-22 15:53:07 -0400

    test1

--- task4.js
+++ task4.js
@@ -1,7 +1,14 @@
 export class Player {
-  constructor() {
-   
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
   }
+  info() {
+    console.log(`${this.name} has reached Level ${this.level}!`);
+  }
+levelUp() {
+    this.level++;
+    console.log(`${this.name} leveled up to Level ${this.level}!`);
   
-  
+  }
 }
