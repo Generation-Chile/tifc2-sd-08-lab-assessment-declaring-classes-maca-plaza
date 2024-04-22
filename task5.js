e3462509ec78f32d9b510b049379e336941845ae
maca-plaza <165829605+maca-plaza@users.noreply.github.com>
2024-04-22 15:54:03 -0400

    test1

--- task5.js
+++ task5.js
@@ -1,8 +1,30 @@
 export class Player {
-  constructor() {
+  constructor(name, level = 1, experience = 0) {
+    this.name = name;
+    this.level = level;
+    this.experience = experience;
+    this.experienceForNextLevel = 100; // Puntos de experiencia requeridos para el próximo nivel
+  }
+    info () {
+      console.log(`${this.name} has reached Level ${this.level}!`);
+    }
+    levelUp() {
+      this.level++;
+      this.experience = 0; // Reiniciar la experiencia después de subir de nivel
+      console.log(`${this.name} leveled up to Level ${this.level}!`);
+  }
+
+  gainExperience(points) {
+      this.experience += points;
+      console.log(`${this.name} gained ${points} experience points.`);
+
+      if (this.experience >= this.experienceForNextLevel) {
+          this.levelUp(); // Subir de nivel si se alcanzan suficientes puntos de experiencia
+      }
+  }
+}
     
-  }
- 
-  }
+    
+  
 
   
